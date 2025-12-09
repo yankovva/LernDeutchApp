b5c19a0d6ecc7e3ebc2545563098f1d9aea82064
yankovva <maggie.yankovaaa@gmail.com>
2025-12-09 17:46:25 +0100

    Creating DbContext class

---
+++ LerningApp.Data/LerningAppContext.cs
@@ -0,0 +1,17 @@
+using LerningApp.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LerningApp.Data;
+
+public class LerningAppContext : DbContext
+{
+    public LerningAppContext(DbContextOptions options)
+    :base(options)
+    {
+        
+    }
+    
+   public virtual DbSet<Lesson> Lessons { get; set; } = null!;
+   
+   public virtual DbSet<Course> Courses { get; set; } = null!;
+}
