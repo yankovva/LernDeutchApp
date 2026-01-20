a34c9de289991deb619cdbfdf373e83d2aa90262
yankovva <maggie.yankovaaa@gmail.com>
2026-01-20 16:53:58 +0100

    adding Migrations

---
+++ LerningApp.Data/Migrations/20260120154657_Level.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LerningApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Level : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Lessons_Courses_CourseId1",
+                table: "Lessons");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Lessons_CourseId1",
+                table: "Lessons");
+
+            migrationBuilder.DropColumn(
+                name: "CourseId1",
+                table: "Lessons");
+
+            migrationBuilder.DropColumn(
+                name: "Level",
+                table: "Courses");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "CourseId",
+                table: "Lessons",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "LevelId",
+                table: "Courses",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "Levels",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(3)", maxLength: 3, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(1500)", maxLength: 1500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Levels", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lessons_CourseId",
+                table: "Lessons",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_LevelId",
+                table: "Courses",
+                column: "LevelId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Courses_Levels_LevelId",
+                table: "Courses",
+                column: "LevelId",
+                principalTable: "Levels",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Lessons_Courses_CourseId",
+                table: "Lessons",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Courses_Levels_LevelId",
+                table: "Courses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Lessons_Courses_CourseId",
+                table: "Lessons");
+
+            migrationBuilder.DropTable(
+                name: "Levels");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Lessons_CourseId",
+                table: "Lessons");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Courses_LevelId",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "LevelId",
+                table: "Courses");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CourseId",
+                table: "Lessons",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CourseId1",
+                table: "Lessons",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Level",
+                table: "Courses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lessons_CourseId1",
+                table: "Lessons",
+                column: "CourseId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Lessons_Courses_CourseId1",
+                table: "Lessons",
+                column: "CourseId1",
+                principalTable: "Courses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
