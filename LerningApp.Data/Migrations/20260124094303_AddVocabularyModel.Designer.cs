a2e16f4af1a022ebff885cb77991d9d31743b247
yankovva <maggie.yankovaaa@gmail.com>
2026-01-24 10:44:40 +0100

    Adding Migration and updating the DB

--- LerningApp.Data/Migrations/20260124094303_AddVocabularyModel.Designer.cs
+++ LerningApp.Data/Migrations/20260124094303_AddVocabularyModel.Designer.cs
@@ -4,6 +4,7 @@ using LerningApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LerningApp.Data.Migrations
 {
     [DbContext(typeof(LerningAppContext))]
-    partial class LerningAppContextModelSnapshot : ModelSnapshot
+    [Migration("20260124094303_AddVocabularyModel")]
+    partial class AddVocabularyModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ LerningApp.Data/Migrations/20260124094303_AddVocabularyModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LerningApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddVocabularyModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VocabularyItems",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    LessonId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VocabularyItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VocabularyItems_Lessons_LessonId",
+                        column: x => x.LessonId,
+                        principalTable: "Lessons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VocabularyTerms",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    VocabularyItemId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Side = table.Column<string>(type: "nvarchar(5)", maxLength: 5, nullable: false),
+                    IsPrimary = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VocabularyTerms", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VocabularyTerms_VocabularyItems_VocabularyItemId",
+                        column: x => x.VocabularyItemId,
+                        principalTable: "VocabularyItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VocabularyItems_LessonId",
+                table: "VocabularyItems",
+                column: "LessonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VocabularyTerms_VocabularyItemId_Side",
+                table: "VocabularyTerms",
+                columns: new[] { "VocabularyItemId", "Side" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VocabularyTerms");
+
+            migrationBuilder.DropTable(
+                name: "VocabularyItems");
+        }
+    }
+}
