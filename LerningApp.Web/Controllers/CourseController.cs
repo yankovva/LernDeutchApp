4d7dd92cc36cc6a0a518c3aa035ede8738a1f832
yankovva <maggie.yankovaaa@gmail.com>
2026-01-26 14:34:15 +0100

    Adding Basic Details page for a course

--- LerningApp.Web/Controllers/CourseController.cs
+++ LerningApp.Web/Controllers/CourseController.cs
@@ -1,6 +1,7 @@
 using LerningApp.Data;
 using LerningApp.Data.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace LerningApp.Controllers;
 
@@ -20,6 +21,27 @@ public class CourseController : Controller
             .Courses
             .ToList();
         
-        return View(courses);
+        return this.View(courses);
+    }
+
+    [HttpGet]
+    public IActionResult Details(string id)
+    {
+        bool isIdValid = Guid.TryParse(id, out Guid Guidid);
+        if (!isIdValid)
+        {
+            return this.RedirectToAction(nameof(this.Index));
+        }
+        
+        Course? course = this._dbcontext
+            .Courses
+            .FirstOrDefault(c=>c.Id == Guidid);
+
+        if (course == null)
+        {
+            return this.RedirectToAction(nameof(this.Index));
+        }
+
+        return this.View(course);
     }
 }
