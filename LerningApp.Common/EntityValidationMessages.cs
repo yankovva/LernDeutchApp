480e293ca5014cd7d2e2a493a9dfb50e0ac7786e
yankovva <maggie.yankovaaa@gmail.com>
2026-01-27 10:06:47 +0100

    Adding EntityValidationMessages.cs and some messages for course creation

---
+++ LerningApp.Common/EntityValidationMessages.cs
@@ -0,0 +1,12 @@
+namespace LerningApp.Common;
+
+public static class EntityValidationMessages
+{
+   public static class Course
+   {
+      public const string CourseLevelValidationMessage = "Моля избери ниво.";
+      public const string CourseNameValidationMessage = "Моля попълнете име на курса.";
+      public const string CourseDescriptionValidationMessage = "Моля попълнете описание на курса.";
+
+   }
+}
--- LerningApp.Web.ViewModels/Course/AddCourseViewModel.cs
+++ LerningApp.Web.ViewModels/Course/AddCourseViewModel.cs
@@ -1,21 +1,22 @@
 using System.ComponentModel.DataAnnotations;
 using LerningApp.Web.ViewModels.Level;
 using static LerningApp.Common.EntityValidationConstants.Course;
+using static LerningApp.Common.EntityValidationMessages.Course;
 namespace LerningApp.Web.ViewModels.Course;
 
 public class AddCourseViewModel
 {
-    [Required]
+    [Required (ErrorMessage = CourseNameValidationMessage)]
     [MinLength(NameMinLength)]
     [MaxLength(NameMaxLength)]
     public string Name { get; set; } = null!;
     
-    [Required]
+    [Required(ErrorMessage = CourseDescriptionValidationMessage)]
     [MinLength(DescriptionMinLength)]
     [MaxLength(DescriptionMaxLength)]
     public string Description { get; set; } = null!;
     
-    [Required(ErrorMessage = "Моля избери ниво.")]
+    [Required(ErrorMessage = CourseLevelValidationMessage)]
     public Guid? LevelId { get; set; }  
 
     public IEnumerable<LevelOptionsViewModel> Levels { get; set; } = new List<LevelOptionsViewModel>();
