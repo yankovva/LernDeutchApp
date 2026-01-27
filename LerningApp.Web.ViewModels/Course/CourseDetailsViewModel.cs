4d6b05f9651e130987aaa6ecb09d25a3ad633cef
yankovva <maggie.yankovaaa@gmail.com>
2026-01-27 19:21:21 +0100

    Adding CourseDetailsViewModel.cs

---
+++ LerningApp.Web.ViewModels/Course/CourseDetailsViewModel.cs
@@ -0,0 +1,13 @@
+namespace LerningApp.Web.ViewModels.Course;
+
+public class CourseDetailsViewModel
+{
+    public string Id { get; set; }
+    
+    public string Name { get; set; } = null!;
+    
+    public string Description { get; set; } = null!;
+    
+    public string LevelName { get; set; } = null!;
+    
+}
