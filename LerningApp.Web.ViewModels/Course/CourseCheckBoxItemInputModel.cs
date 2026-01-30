4d3a01feb93344b5991764f449ee74a82324c003
yankovva <maggie.yankovaaa@gmail.com>
2026-01-30 11:52:04 +0100

    Adding 2 view models for adding lesson to course

---
+++ LerningApp.Web.ViewModels/Course/CourseCheckBoxItemInputModel.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+using static LerningApp.Common.EntityValidationConstants.Course;
+namespace LerningApp.Web.ViewModels.Course;
+
+public class CourseCheckBoxItemInputModel
+{
+    [Required]
+    public string CourseId { get; set; } = null!;
+    [Required]
+    [MinLength(NameMinLength)]
+    [MaxLength(NameMaxLength)]
+    public string CourseName { get; set; } = null!;
+    
+    public bool IsChecked { get; set; }
+}
---
+++ LerningApp.Web.ViewModels/Lesson/AddLessonToCourseViewModel.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using LerningApp.Web.ViewModels.Course;
+
+using static LerningApp.Common.EntityValidationConstants.Lesson;
+namespace LerningApp.Web.ViewModels.Lesson;
+
+public class AddLessonToCourseViewModel
+{
+    [Required]
+    public string LessonId { get; set; } = null!;
+    
+    [Required]
+    [MinLength(NameMinLength)]
+    [MaxLength(NameMaxLength)]
+    public string LessonName { get; set; } = null!;
+    
+    public List<CourseCheckBoxItemInputModel> Courses { get; set; }
+        = new List<CourseCheckBoxItemInputModel>();
+}
