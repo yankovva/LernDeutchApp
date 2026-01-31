1af2c2689b00e685a39ddd609655ca8a67a8f9cb
yankovva <maggie.yankovaaa@gmail.com>
2026-01-31 10:07:42 +0100

    Creating AddLessonInputModel.cs for creating a new Lesson

---
+++ LerningApp.Web.ViewModels/Lesson/AddLessonInputModel.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using LerningApp.Web.ViewModels.Course;
+using static LerningApp.Common.EntityValidationConstants.Lesson;
+namespace LerningApp.Web.ViewModels.Lesson;
+
+public class AddLessonInputModel
+{
+    [Required]
+    [MinLength(NameMinLength)]
+    [MaxLength(NameMaxLength)]
+    public string Name { get; set; } = null!;
+
+    [Required]
+    [MinLength(ContentMinLength)]
+    [MaxLength(ContentMaxLength)]
+    public string Content { get; set; } = null!;
+
+    [Range(OrderIndexMin, OrderIndexMax)]
+    public int OrderIndex { get; set; }
+
+    // optional
+    public string? CourseId { get; set; }
+
+    public List<CourseOptionsViewModel> Courses { get; set; }
+        = new List<CourseOptionsViewModel>();
+}
