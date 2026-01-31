15fe809837064366fae3f618f3454093d81d3d07
yankovva <maggie.yankovaaa@gmail.com>
2026-01-31 10:08:08 +0100

    Creating CourseOptionsViewModel.cs to show all available Courses options

---
+++ LerningApp.Web.ViewModels/Course/CourseOptionsViewModel.cs
@@ -0,0 +1,8 @@
+namespace LerningApp.Web.ViewModels.Course;
+
+public class CourseOptionsViewModel
+{
+    public string Id { get; set; } = null!;
+    
+    public string Name { get; set; } = null!;
+}
