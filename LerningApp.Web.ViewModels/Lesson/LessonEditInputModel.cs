d6c00ac1c2896bfaf5cf99dcd2592e949650bcfd
yankovva <maggie.yankovaaa@gmail.com>
2026-02-03 14:47:56 +0100

    Adding a LessonEditInputModel.cs

---
+++ LerningApp.Web.ViewModels/Lesson/LessonEditInputModel.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+using LerningApp.Web.ViewModels.Course;
+using static LerningApp.Common.EntityValidationConstants.Lesson;
+
+namespace LerningApp.Web.ViewModels.Lesson;
+
+public class LessonEditInputModel
+{
+    public string Id { get; set; } = null!;
+
+    [Required]
+    [MinLength(NameMinLength)]
+    [MaxLength(NameMaxLength)]
+    public string Name { get; set; } = null!;
+
+    [Required]
+    [MinLength(ContentMinLength)]
+    [MaxLength(ContentMaxLength)]
+    public string Content { get; set; } = null!;
+
+    [Range(OrderIndexMin, OrderIndexMax)]
+    public int OrderIndex { get; set; }
+
+    // optional: 
+    public string? CourseId { get; set; }
+
+    public IList<CourseOptionsViewModel> Courses { get; set; }
+        = new List<CourseOptionsViewModel>();
+}
