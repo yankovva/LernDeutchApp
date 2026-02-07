6dd301c7f7918d8ba1d0127deb1412bffe58c510
yankovva <maggie.yankovaaa@gmail.com>
2026-02-07 18:48:32 +0100

    Creating the UsersCoursesController.cs and adding Index page logic

---
+++ LerningApp.Web/Controllers/UsersCoursesController.cs
@@ -0,0 +1,48 @@
+using System.Security.Claims;
+using LerningApp.Data;
+using LerningApp.Data.Models;
+using LerningApp.Web.ViewModels.UsersCouses;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LerningApp.Controllers;
+
+[Authorize]
+public class UsersCoursesController(LerningAppContext dbcontext, UserManager<ApplicationUser> userManager)
+    : BaseController
+{
+ 
+    [Authorize]
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+         var userId =  userManager.GetUserId(User);
+
+         if (string.IsNullOrEmpty(userId))
+         {
+             return this.RedirectToAction("/Identity/Account/Login");
+         }
+
+         List<MyCourseCardViewModel> courses =  await dbcontext.UsersCourses
+             .AsNoTracking()
+             .Include(uc => uc.Course)
+             .ThenInclude(c => c.Level)
+             .Where(uc => uc.UserId.ToString() == userId)
+             .Select(uc => new MyCourseCardViewModel
+             {
+                 Id = uc.Course.Id.ToString(),
+                 Name = uc.Course.Name,
+                 Description = uc.Course.Description,
+                 LevelName = uc.Course.Level.Name,
+                 StartedAt = uc.StartedAt,
+                 CompletedAt = uc.CompletedAt
+             })
+             .ToListAsync();
+         
+         var model = new UsersCoursesIndexViewModel { Courses = courses };
+         
+        return View(model);
+    }
+}
