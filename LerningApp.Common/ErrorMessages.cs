2c8b7014bc86b399075d577e94dac493fc6634f8
yankovva <maggie.yankovaaa@gmail.com>
2026-02-11 15:43:21 +0100

    Adding ErrorMessages.cs

---
+++ LerningApp.Common/ErrorMessages.cs
@@ -0,0 +1,9 @@
+namespace LerningApp.Common;
+
+public class ErrorMessages
+{
+    public const string SuccessMessage = "SuccessMessage";
+    public const string WarningMessage = "WarningMessage";
+    public const string ErrorMessage = "ErrorMessage";
+    public const string InfoMessage = "InfoMessage";
+}
