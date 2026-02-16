7a75df0150e1b675c8ff2842f9e5974af55941d9
yankovva <maggie.yankovaaa@gmail.com>
2026-02-16 12:59:21 +0200

    Creating ICourseService.cs

---
+++ LerningApp.Services.Data/Interfaces/ICourseService.cs
@@ -0,0 +1,12 @@
+using LerningApp.Web.ViewModels.Course;
+
+namespace LerningApp.Services.Data.Interfaces;
+
+public interface ICourseService
+{ 
+    Task<IEnumerable<CourseIndexViewModel>> IndexGetCoursesAsync();
+    
+    Task AddCourseAsync(AddCourseViewModel model);
+    
+    Task<CourseDetailsViewModel> GetCourseDetailsByIdAsync(Guid id);
+}
