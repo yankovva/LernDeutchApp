3d741575e8af1f915f9fe56e2048d35b1346f6d2
yankovva <maggie.yankovaaa@gmail.com>
2026-02-16 14:37:36 +0200

    Creating ILevelService.cs

---
+++ LerningApp.Services.Data/Interfaces/ILevelService.cs
@@ -0,0 +1,8 @@
+using LerningApp.Web.ViewModels.Level;
+
+namespace LerningApp.Services.Data.Interfaces;
+
+public interface ILevelService
+{
+    Task<List<LevelOptionsViewModel>> GetAllLevelsFromDbAsync();
+}
