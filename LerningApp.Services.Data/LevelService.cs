7c310155b767e21d68ca9e140ec3a30f2e841bf1
yankovva <maggie.yankovaaa@gmail.com>
2026-02-16 14:38:02 +0200

    Adding LevelService with a methid that gives all levels

---
+++ LerningApp.Services.Data/LevelService.cs
@@ -0,0 +1,27 @@
+using LerningApp.Common;
+using LerningApp.Data.Models;
+using LerningApp.Data.Repository.Interfaces;
+using LerningApp.Services.Data.Interfaces;
+using LerningApp.Web.ViewModels.Level;
+using Microsoft.EntityFrameworkCore;
+
+namespace LerningApp.Services.Data;
+
+public class LevelService(IRepository<Level, Guid>levelRepository) : ILevelService
+{
+    public async Task<List<LevelOptionsViewModel>> GetAllLevelsFromDbAsync()
+    {
+        var levels = await levelRepository
+            .GetAllAttached()
+            .AsNoTracking()
+            .OrderBy(c => c.Name)
+            .Select(c => new LevelOptionsViewModel
+            {
+                Id = c.Id.ToString(), 
+                Name = c.Name
+            })
+            .ToListAsync();
+        
+        return levels;
+    }
+}
