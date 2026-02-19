db16560d2e13ce637c8ac3e263c9bfcc6775fa98
yankovva <maggie.yankovaaa@gmail.com>
2026-02-19 12:39:56 +0200

    Making the VocabularyCardController.cs work with the Vocabulary Service

--- LerningApp.Web/Controllers/VocabularyCardController.cs
+++ LerningApp.Web/Controllers/VocabularyCardController.cs
@@ -1,56 +1,27 @@
 using LerningApp.Data;
 using LerningApp.Data.Models;
+using LerningApp.Services.Data.Interfaces;
 using LerningApp.Web.ViewModels.VocabularyCard;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace LerningApp.Controllers;
 
-public class VocabularyCardController(LerningAppContext dbcontext) :BaseController
+public class VocabularyCardController(LerningAppContext dbcontext,
+    IVocabularyCardService vocabularyCardService) :BaseController
 {
     [HttpGet]
     public async Task<IActionResult> Index(string lessonId)
     {
-        Guid lessonGuidId = Guid.Empty;
-        if (!this.IsGuidValid(lessonId, ref lessonGuidId))
-        {
-            TempData["ErrorMessage"] = "Урокът не е намерен.";
-            return this.RedirectToAction(nameof(this.Index));
-        }
-        
-        var lesson = await dbcontext.Lessons
-            .AsNoTracking()
-            .FirstOrDefaultAsync(l => l.Id == lessonGuidId);
+        var result = await vocabularyCardService.IndexGetAllCardsForALessonAsync(lessonId);
 
-        if (lesson == null)
+        if (result.Result == false)
         {
-            TempData["ErrorMessage"] = "Урокът не е намерен.";
+            TempData["ErrorMessage"] = result.Message;
             return RedirectToAction("Index", "Lesson");
         }
-           
-
-        var cards = await dbcontext.VocabularyCards
-            .AsNoTracking()
-            .Include(v => v.PartOfSpeech)
-            .Include(v => v.Terms)
-            .Where(v => v.LessonId == lessonGuidId)
-            .Select(v => new VocabularyCardRowViewModel
-            {
-                Id = v.Id.ToString(),
-                German = v.Terms.FirstOrDefault(t => t.Side == "de" && t.IsPrimary)!.Word,
-                PartOfSpeech = v.PartOfSpeech.Name,
-                Gender = v.Terms.FirstOrDefault(t => t.Side == "de" && t.IsPrimary)!.Gender ?? "-",
-            })
-            .ToListAsync();
-
-        var model = new VocabularyCardsIndexViewModel
-        {
-            LessonId = lesson.Id.ToString(),
-            LessonName = lesson.Name,
-            Cards = cards
-        };
-
-        return View(model);
+        
+        return View(result.Data);
     }
 
     [HttpGet]
@@ -96,7 +67,7 @@ public class VocabularyCardController(LerningAppContext dbcontext) :BaseControll
             Gender = card.Terms.FirstOrDefault(t => t.Side == "de" && t.IsPrimary)?.Gender ?? "-",
             ExampleSentence = card.Terms.FirstOrDefault(t => t.Side == "de" && t.IsPrimary)?.ExampleSentence ?? "-",
         };
-        return this.View(model);
         
+        return this.View(model);
     }
 }
