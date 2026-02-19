20eb2e6a06c50f90392f4e3ae46b49e0242acf67
yankovva <maggie.yankovaaa@gmail.com>
2026-02-19 12:56:59 +0200

    Making the Index Action work with the IUsersCoursesService

--- LerningApp.Web/Controllers/UsersCoursesController.cs
+++ LerningApp.Web/Controllers/UsersCoursesController.cs
@@ -1,43 +1,30 @@
-using System.Security.Claims;
-using LerningApp.Data;
 using LerningApp.Data.Models;
-using LerningApp.Web.ViewModels.UsersCouses;
+using LerningApp.Services.Data.Interfaces;
+
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace LerningApp.Controllers;
 
 [Authorize]
-public class UsersCoursesController(LerningAppContext dbcontext, UserManager<ApplicationUser> userManager)
+public class UsersCoursesController(IUsersCoursesService usersCoursesService, 
+    UserManager<ApplicationUser> userManager)
     : BaseController
 {
  
     [Authorize]
     [HttpGet]
     public async Task<IActionResult> Index()
-    {
-         var userId =  userManager.GetUserId(User)!;
-
-         List<MyCourseCardViewModel> courses =  await dbcontext.UsersCourses
-             .AsNoTracking()
-             .Include(uc => uc.Course)
-             .ThenInclude(c => c.Level)
-             .Where(uc => uc.UserId.ToString() == userId)
-             .Select(uc => new MyCourseCardViewModel
-             {
-                 Id = uc.Course.Id.ToString(),
-                 Name = uc.Course.Name,
-                 Description = uc.Course.Description,
-                 LevelName = uc.Course.Level.Name,
-                 StartedAt = uc.StartedAt,
-                 CompletedAt = uc.CompletedAt
-             })
-             .ToListAsync();
-         
-         var model = new UsersCoursesIndexViewModel { Courses = courses };
-         
-        return View(model);
+    {  
+        var userId =  userManager.GetUserId(User)!;
+        var result = await usersCoursesService.IndexGetAllUsersCoursesAsync(userId);
+        if (result.Result == false)
+        {
+           TempData["ErrorMessage"] = result.Message;
+           return RedirectToAction("Index", "Course");
+        }
+        
+        return View(result.Data);
     }
 }
