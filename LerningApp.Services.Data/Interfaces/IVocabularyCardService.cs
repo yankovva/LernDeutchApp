7108cd4cb04c451d6dcda1a0f267bfe1dd1f77c0
yankovva <maggie.yankovaaa@gmail.com>
2026-02-19 12:38:33 +0200

    Creating interface for the VocabularyCardService

---
+++ LerningApp.Services.Data/Interfaces/IVocabularyCardService.cs
@@ -0,0 +1,13 @@
+using LerningApp.Common;
+using LerningApp.Web.ViewModels.VocabularyCard;
+
+namespace LerningApp.Services.Data.Interfaces;
+
+public interface IVocabularyCardService
+{
+    Task<ServiceResultT<VocabularyCardsIndexViewModel>> IndexGetAllCardsForALessonAsync(string lessonId);
+    
+    Task<ServiceResultT<VocabularyCardDetailsViewModel>> GetDetailsForACardAsync(string id);
+
+
+}
