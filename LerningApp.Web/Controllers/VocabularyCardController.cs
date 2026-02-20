3192265f0aa03e1aec9282406ae5481d4c24eca8
yankovva <maggie.yankovaaa@gmail.com>
2026-02-20 14:42:36 +0200

    Adding the logic for the creation via the Service

--- LerningApp.Web/Controllers/VocabularyCardController.cs
+++ LerningApp.Web/Controllers/VocabularyCardController.cs
@@ -7,7 +7,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace LerningApp.Controllers;
 
-public class VocabularyCardController(IVocabularyCardService vocabularyCardService) :BaseController
+public class VocabularyCardController(IVocabularyCardService vocabularyCardService,
+    IPartOfSpeechService partOfSpeechService,
+    LerningAppContext dbContext ) :BaseController
 {
     [HttpGet]
     public async Task<IActionResult> Index(string lessonId)
@@ -35,5 +37,37 @@ public class VocabularyCardController(IVocabularyCardService vocabularyCardServi
         }
         
         return this.View(result.Data);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Create(string lessonId)
+    {
+        var model = new VocabularyCardCreateInputModel()
+        {
+            LessonId = lessonId,
+            PartOfSpeechOptions = await partOfSpeechService.GetAllPartOfSpeechOptionsAsync()
+        };
+
+        return this.View(model);
+    }
+    [HttpPost]
+    public async Task<IActionResult> Create(VocabularyCardCreateInputModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            model.PartOfSpeechOptions = await partOfSpeechService.GetAllPartOfSpeechOptionsAsync();
+            return View(model);
+        }
+
+        var result = await vocabularyCardService.CreateVocabularyCardAsync(model);
+        if (result.Result == false)
+        {
+            TempData["ErrorMessage"] = result.Message;
+            model.PartOfSpeechOptions = await partOfSpeechService.GetAllPartOfSpeechOptionsAsync();
+            return View(model);
+        }
+        
+        TempData["SuccessMessage"] = "Успешно създадохте нова карта";
+        return RedirectToAction(nameof(Index), new { lessonId = model.LessonId });
     }
 }
