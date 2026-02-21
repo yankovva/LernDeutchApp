b8036f4b6e82d002824ecb10da7873091c9f4729
yankovva <maggie.yankovaaa@gmail.com>
2026-02-21 12:28:12 +0200

    Creating VocabularyCardEditInputModel.cs

---
+++ LerningApp.Web.ViewModels/VocabularyCard/VocabularyCardEditInputModel.cs
@@ -0,0 +1,49 @@
+using System.ComponentModel.DataAnnotations;
+using LerningApp.Web.ViewModels.PartOfSpeech;
+using Microsoft.AspNetCore.Http;
+
+namespace LerningApp.Web.ViewModels.VocabularyCard;
+
+public class VocabularyCardEditInputModel
+{
+    [Required]
+    public string Id { get; set; } = null!;
+    
+    [Required]
+    public string LessonId { get; set; } = null!;
+    
+    [Required]
+    [MaxLength(63)]
+    [MinLength(2)]
+    public string GermanWord { get; set; } = null!;
+    
+    [MaxLength(45)]
+    [MinLength(1)]
+    [Required]
+    public string EnglishWord { get; set; } = null!;
+    
+    [MaxLength(39)]
+    [MinLength(1)]
+    [Required]
+    public string BulgarianWord { get; set; } = null!;
+    
+    [MaxLength(500)]
+    public string? ImageUrl { get; set; } 
+    
+    public IFormFile Image { get; set; } = null!;
+    
+    [MaxLength(3)]
+    //der die das
+    public string? Gender { get; set; }
+    
+    [MaxLength(500)]
+    public string? ExampleSentence { get; set; }
+
+    public bool IsPrimary { get; set; }
+    
+    [Required]
+    public string PartOfSpeechId { get; set; } = null!;
+
+    public IList<PartOfSpeechOptionsViewModel> PartOfSpeechOptions { get; set; } 
+        = new List<PartOfSpeechOptionsViewModel>();
+}
