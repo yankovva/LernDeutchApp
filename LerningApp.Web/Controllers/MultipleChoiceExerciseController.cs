065fd450a58a972e90d3ffa210b31c781a881733
yankovva <maggie.yankovaaa@gmail.com>
2026-02-25 14:20:25 +0200

    Creating the controller with the logic for create and check the answer logic

---
+++ LerningApp.Web/Controllers/MultipleChoiceExerciseController.cs
@@ -0,0 +1,104 @@
+using LerningApp.Common;
+using LerningApp.Data;
+using LerningApp.Data.Models;
+using LerningApp.Web.ViewModels.MultipleChoiceExercise;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LerningApp.Controllers;
+
+[Authorize]
+public class MultipleChoiceExerciseController(LerningAppContext dbContext, UserManager<ApplicationUser> userManager) : Controller
+{
+    
+    [HttpGet]
+    public IActionResult Create(string lessonId)
+    {
+        var model = new CreateMultipleChoiceExerciseViewModel()
+        {
+          LessonId = lessonId
+        };
+        
+        return View(model);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(CreateMultipleChoiceExerciseViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+        
+        string currentUserId = userManager.GetUserId(User)!;
+        
+        Guid lessonId = Guid.Empty;
+        if (!string.IsNullOrWhiteSpace(model.LessonId))
+        {
+            if (!Guid.TryParse(model.LessonId, out  lessonId))
+            {
+                ModelState.AddModelError("", "Невалиден урок.");
+                return View(model);
+            }
+            
+            Lesson? lesson = await dbContext
+                .Lessons
+                .FirstOrDefaultAsync(l=> l.Id == lessonId);
+            
+            if (lesson == null)
+            {
+                ModelState.AddModelError("", "Невалиден урок.");
+                return View(model);
+            }
+        }
+
+        MultipleChoiceExercise exercise = new MultipleChoiceExercise()
+        {
+            LessonId = lessonId,
+            Question = model.Question,
+            CorrectAnswer = model.CorrectAnswer,
+            SecondWrongAnswer = model.SecondWrongAnswer,
+            FirstWrongAnswer = model.FirstWrongAnswer,
+            ThirdWrongAnswer = model.ThirdWrongAnswer ?? null,
+            OrderIndex = model.OrderIndex,
+            PublisherId = Guid.Parse(currentUserId),
+        };
+        
+        await dbContext.MultipleChoiceExercises.AddAsync(exercise);
+        await dbContext.SaveChangesAsync();
+        
+        TempData["SuccessMessage"] = "Успешно създадохте упражнението";
+        return RedirectToAction(nameof(Create), new { lessonId = model.LessonId });
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> CheckMultipleChoice(string exerciseId, string lessonId, string selectedAnswer)
+    {
+        if (!Guid.TryParse(exerciseId, out var exId))
+        {
+            TempData["ErrorMessage"] = "Невалидно упражнение.";
+            return RedirectToAction("Details", "Lesson" ,new { id = lessonId });
+        }
+        
+        var exercise = await dbContext
+            .MultipleChoiceExercises
+            .FirstOrDefaultAsync(ex => ex.Id == Guid.Parse(exerciseId));
+
+        if (exercise == null)
+        {
+            TempData["ErrorMessage"] = "Упражнението не е намерено.";
+            return RedirectToAction("Details", "Lesson" ,new { id = lessonId });
+        }
+        if (exercise.CorrectAnswer == selectedAnswer)
+        {
+            TempData["SuccessMessage"] = "Правилен отговор";
+        } else
+            TempData["ErrorMessage"] = "Грешен отговор.";
+        
+        return RedirectToAction("Details", "Lesson" ,new { id = lessonId });
+    }
+}
