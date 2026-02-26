945ca2a309df741773169ce68b2cc8014f5d4a0c
yankovva <maggie.yankovaaa@gmail.com>
2026-02-26 13:41:53 +0200

    Updating the view and adding the IndexTranslationExerciseViewModel

---
+++ LerningApp.Web.ViewModels/TranslationExercise/IndexTranslationExerciseViewModel.cs
@@ -0,0 +1,14 @@
+namespace LerningApp.Web.ViewModels.TranslationExercise;
+
+public class IndexTranslationExerciseViewModel
+{
+    public string Id { get; set; } = null!;
+
+    public string GermanSentence { get; set; } = null!;
+    
+    public string EnglishSentence { get; set; } = null!;
+    
+    public string BulgarianSentence { get; set; }  = null!;
+
+    public int OrderIndex { get; set; }
+}
