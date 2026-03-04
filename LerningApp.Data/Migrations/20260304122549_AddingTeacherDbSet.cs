b88583cecbbd987a6d78e9dc48ba2818853726df
yankovva <maggie.yankovaaa@gmail.com>
2026-03-04 13:27:49 +0100

    Adding Teacher Table

---
+++ LerningApp.Data/Migrations/20260304122549_AddingTeacherDbSet.cs
@@ -0,0 +1,174 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LerningApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingTeacherDbSet : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Courses_Teacher_PublisherId",
+                table: "Courses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Lessons_Teacher_PublisherId",
+                table: "Lessons");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_MultipleChoiceExercises_Teacher_PublisherId",
+                table: "MultipleChoiceExercises");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Teacher_AspNetUsers_UserId",
+                table: "Teacher");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TranslationExercises_Teacher_PublisherId",
+                table: "TranslationExercises");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Teacher",
+                table: "Teacher");
+
+            migrationBuilder.RenameTable(
+                name: "Teacher",
+                newName: "Teachers");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Teacher_UserId",
+                table: "Teachers",
+                newName: "IX_Teachers_UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Teachers",
+                table: "Teachers",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Courses_Teachers_PublisherId",
+                table: "Courses",
+                column: "PublisherId",
+                principalTable: "Teachers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Lessons_Teachers_PublisherId",
+                table: "Lessons",
+                column: "PublisherId",
+                principalTable: "Teachers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MultipleChoiceExercises_Teachers_PublisherId",
+                table: "MultipleChoiceExercises",
+                column: "PublisherId",
+                principalTable: "Teachers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Teachers_AspNetUsers_UserId",
+                table: "Teachers",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TranslationExercises_Teachers_PublisherId",
+                table: "TranslationExercises",
+                column: "PublisherId",
+                principalTable: "Teachers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Courses_Teachers_PublisherId",
+                table: "Courses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Lessons_Teachers_PublisherId",
+                table: "Lessons");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_MultipleChoiceExercises_Teachers_PublisherId",
+                table: "MultipleChoiceExercises");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Teachers_AspNetUsers_UserId",
+                table: "Teachers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TranslationExercises_Teachers_PublisherId",
+                table: "TranslationExercises");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Teachers",
+                table: "Teachers");
+
+            migrationBuilder.RenameTable(
+                name: "Teachers",
+                newName: "Teacher");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Teachers_UserId",
+                table: "Teacher",
+                newName: "IX_Teacher_UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Teacher",
+                table: "Teacher",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Courses_Teacher_PublisherId",
+                table: "Courses",
+                column: "PublisherId",
+                principalTable: "Teacher",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Lessons_Teacher_PublisherId",
+                table: "Lessons",
+                column: "PublisherId",
+                principalTable: "Teacher",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MultipleChoiceExercises_Teacher_PublisherId",
+                table: "MultipleChoiceExercises",
+                column: "PublisherId",
+                principalTable: "Teacher",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Teacher_AspNetUsers_UserId",
+                table: "Teacher",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TranslationExercises_Teacher_PublisherId",
+                table: "TranslationExercises",
+                column: "PublisherId",
+                principalTable: "Teacher",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
