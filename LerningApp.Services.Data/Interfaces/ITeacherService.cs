3bbd5bbbcae5829ceab2c5c2f3dcd272826b57b7
yankovva <maggie.yankovaaa@gmail.com>
2026-03-04 13:42:21 +0100

    Adding IsUserTeacherAsync method

--- LerningApp.Services.Data/Interfaces/ITeacherService.cs
+++ LerningApp.Services.Data/Interfaces/ITeacherService.cs
@@ -5,5 +5,6 @@ namespace LerningApp.Services.Data.Interfaces;
 
 public interface ITeacherService 
 {
+    Task<bool> IsUserTeacherAsync(string userId);
     Task<Guid?> GetTeacherIdAsync(string userId);
 }
--- LerningApp.Services.Data/TeacherService.cs
+++ LerningApp.Services.Data/TeacherService.cs
@@ -8,16 +8,30 @@ namespace LerningApp.Services.Data;
 
 public class TeacherService(IRepository<Teacher, Guid> teacherRepository) : ITeacherService
 {
+    public async Task<bool> IsUserTeacherAsync(string userId)
+    {
+        bool result = await  teacherRepository
+            .GetAllAttached()
+            .AnyAsync(t => t.Id.ToString().ToLower() == userId.ToLower() && t.IsApproved == true);
+
+        return result;
+    }
+
+
     public async Task<Guid?> GetTeacherIdAsync(string userId)
     {
         if (!Guid.TryParse(userId, out var userGuid))
         {
             return null;
         }
-        
         Teacher? teacher = await teacherRepository
-            .FirstorDefaultAsync(t => t.UserId == userGuid);
+            .FirstorDefaultAsync(t => t.UserId == userGuid && t.IsApproved == true);
+
+        if (teacher == null)
+        {
+            return null;
+        }
         
-        return teacher?.Id;
+        return teacher.Id;
     }
 }
