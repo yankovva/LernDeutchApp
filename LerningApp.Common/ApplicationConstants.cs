4574b562a91e83a94112a922ba9524e9098a62c1
yankovva <maggie.yankovaaa@gmail.com>
2026-03-12 18:36:35 +0100

    Adding const Path for the Listeningexercise Audio folder

--- LerningApp.Common/ApplicationConstants.cs
+++ LerningApp.Common/ApplicationConstants.cs
@@ -4,6 +4,8 @@ public class ApplicationConstants
 {
     public const string DefaultCardImagePath = "/images/VocabularyCardsImages/defaultcardimage.png";
     public const string DefaultCardDirectoryPath = "images/VocabularyCardsImages";
+    public const string DefaultaListeningExerciseAudiosPath = "audios/ListeningExerciseAudios";
+
     public static readonly string[] AllowedImageExtensions = [".jpg", ".jpeg", ".png", ".gif", ".bmp"];
     public const long MaxFileSize = 5 * 1024 * 1024;
 }
