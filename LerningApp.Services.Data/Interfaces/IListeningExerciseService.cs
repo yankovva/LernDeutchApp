3f8c5d41206f75b56f221772fda92e613a3d7715
yankovva <maggie.yankovaaa@gmail.com>
2026-03-13 15:19:56 +0100

    Adding IListeningExerciseService

---
+++ LerningApp.Services.Data/Interfaces/IListeningExerciseService.cs
@@ -0,0 +1,14 @@
+using LerningApp.Common;
+using LerningApp.Web.ViewModels.ListeningExercise;
+
+namespace LerningApp.Services.Data.Interfaces;
+
+public interface IListeningExerciseService
+{
+     Task<ServiceResultT<CreateListeningExerciseViewModel>> CreateGetListeningExercise(string lessonId, string userId);
+     
+     Task<ServiceResult> CreatePostListeningExercise(CreateListeningExerciseViewModel model, string userId);
+     
+     public Task<(bool isCorrect, string correctAnswer)?> CheckListeningExerciseAnswer(string exerciseId, string selectedAnswer);
+     
+}
