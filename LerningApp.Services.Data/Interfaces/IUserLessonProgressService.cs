c38cd98a37fd67b2ed09f83a9796b1aad375211f
yankovva <maggie.yankovaaa@gmail.com>
2026-03-15 18:32:38 +0100

    Adding method to calculate user progress percentage for a course

--- LerningApp.Services.Data/Interfaces/IUserLessonProgressService.cs
+++ LerningApp.Services.Data/Interfaces/IUserLessonProgressService.cs
@@ -7,4 +7,5 @@ namespace LerningApp.Services.Data.Interfaces;
 public interface IUserLessonProgressService
 {
     Task<ServiceResultT<IndexUserLessonProgressViewModel>> GetUserLessonProgress(Guid lessonId, string? userId);
+    Task<ServiceResultT<int>> GetCourseProgressPercent(Guid courseId, Guid userId);
 }
--- LerningApp.Services.Data/UserLessonProgressService.cs
+++ LerningApp.Services.Data/UserLessonProgressService.cs
@@ -5,6 +5,9 @@ using LerningApp.Services.Data.Interfaces;
 using LerningApp.Web.ViewModels.UserLessonProgress;
 using Microsoft.EntityFrameworkCore;
 
+using static LerningApp.Common.EntityErrorMessages.Common;
+using static LerningApp.Common.EntityErrorMessages.Course;
+
 namespace LerningApp.Services.Data;
 
 public class UserLessonProgressService(IRepository<Lesson,Guid> lessonRepository,
@@ -54,5 +57,27 @@ public class UserLessonProgressService(IRepository<Lesson,Guid> lessonRepository
             IsCompleted = false,
             CompletedAt = null
         });
+    }
+
+    public async Task<ServiceResultT<int>> GetCourseProgressPercent(Guid courseId, Guid userId)
+    {
+        var totalLessons = await lessonRepository
+            .GetAllAttached()
+            .Where(l => l.CourseId == courseId)
+            .CountAsync();
+
+        if (totalLessons == 0)
+        {
+            return ServiceResultT<int>.Success(0);
+        }
+        
+        var completedLessons = await userProgressRepository
+            .GetAllAttached()
+            .Where(up => up.IsCompleted && up.UserId == userId && up.Lesson.CourseId == courseId)
+            .CountAsync();
+        
+        var percentInt = (int)Math.Round((completedLessons / (double)totalLessons) * 100);
+        
+        return ServiceResultT<int>.Success(percentInt);
     }
 }
