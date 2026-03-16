dc78080bf569dd11c91ead1d122bd96e13c285d2
yankovva <maggie.yankovaaa@gmail.com>
2026-03-16 14:02:25 +0100

    Adding a method to check if a lesson is unlocked for the user

--- LerningApp.Web.ViewModels/UserLessonProgress/IndexUserLessonProgressViewModel.cs
+++ LerningApp.Web.ViewModels/UserLessonProgress/IndexUserLessonProgressViewModel.cs
@@ -3,7 +3,6 @@ namespace LerningApp.Web.ViewModels.UserLessonProgress;
 public class IndexUserLessonProgressViewModel
 {
     public bool IsCompleted { get; set; }
-    
     public bool IsUnlocked { get; set; }
     public string? CompletedAt { get; set; }
 }
