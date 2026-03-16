7c28af359f4250c97cc76361fb01c08be7b5e78c
yankovva <maggie.yankovaaa@gmail.com>
2026-03-16 16:09:19 +0100

    register Identity via the new method in the container

--- LerningApp.Web/Program.cs
+++ LerningApp.Web/Program.cs
@@ -19,6 +19,7 @@ using NoOpEmailSender = LerningApp.Web.Infrastructure.NoOpEmailSender;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApplicationDbContext(builder.Configuration);
+builder.Services.AddApplicationIdentity(builder.Configuration);
 
 builder.Services.ConfigureApplicationCookie(options =>
 {
