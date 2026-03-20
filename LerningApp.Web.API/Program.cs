94bbaf2e6f23c444ac57bba8487a7fe527b7da76
yankovva <maggie.yankovaaa@gmail.com>
2026-03-20 18:01:07 +0100

    adding program.cs

---
+++ LerningApp.Web.API/Program.cs
@@ -0,0 +1,70 @@
+using LerningApp.Services.Data;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddApplicationDbContext(builder.Configuration);
+builder.Services.AddApplicationIdentity(builder.Configuration);
+
+builder.Services.RegisterRepositories();
+builder.Services.RegisterUserDefinedServices(typeof(CourseService).Assembly);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDataProtection()
+        .PersistKeysToFileSystem(new DirectoryInfo("/Users/magdalenaqnkova/RiderProjects/LerningApp/.keys"))
+        .SetApplicationName("LerningApp.SharedAuth");
+
+builder.Services.AddCors(options =>
+{
+        options.AddPolicy("WebClient", policy =>
+        {
+                policy.WithOrigins("https://localhost:7222")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials();
+        });
+});
+
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+        options.LoginPath = "/Identity/Account/Login";
+        options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+        options.LogoutPath = "/Identity/Account/Logout";
+        options.ExpireTimeSpan = TimeSpan.FromHours(12);
+        options.SlidingExpiration = true;
+        options.Cookie.HttpOnly = true;
+        options.Cookie.Name = "DeutchBuddy.Auth";
+        options.Cookie.Path = "/";
+        options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+        options.Cookie.SameSite = SameSiteMode.Lax;
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{ 
+        app.UseSwagger();
+        app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("WebClient");
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
