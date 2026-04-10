9eb32dda3ffd6b5f29989a5a2cca20f5c88ec221
yankovva <maggie.yankovaaa@gmail.com>
2026-04-10 16:46:10 +0200

    allow admin access in lesson lifecycle service

--- LerningApp.Services.Data/LessonServices/LessonLifecycleService.cs
+++ LerningApp.Services.Data/LessonServices/LessonLifecycleService.cs
@@ -3,17 +3,19 @@ using LerningApp.Data.Models;
 using LerningApp.Data.Repository.Interfaces;
 using LerningApp.Services.Data.Interfaces;
 using LerningApp.Services.Data.Interfaces.LessonInterfaces;
-using LerningApp.Web.ViewModels.Lesson;
 
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 using static LerningApp.Common.EntityErrorMessages.Lesson;
 using static LerningApp.Common.EntityErrorMessages.Common;
+using static LerningApp.Common.ApplicationConstants;
 
 namespace LerningApp.Services.Data.LessonServices;
 
 public class LessonLifecycleService(IRepository<Lesson, Guid> lessonRepository,
-    ITeacherService teacherService) : ILessonLifecycleService
+    ITeacherService teacherService,
+    UserManager<ApplicationUser> userManager) : ILessonLifecycleService
 {
     public async Task<ServiceResult> SoftDeleteLessonAsync(string id, string userId)
     {
@@ -32,7 +34,10 @@ public class LessonLifecycleService(IRepository<Lesson, Guid> lessonRepository,
         }
         
         Guid? teacherId = await teacherService.GetTeacherIdAsync(userId);
-        if (teacherId == null || teacherId != lesson.PublisherId)
+        var user = await userManager.FindByIdAsync(userId);
+        bool isAdmin = await userManager.IsInRoleAsync(user!, AdminRole);
+        
+        if (!isAdmin && (teacherId == null || lesson.PublisherId != teacherId))
         {
             return ServiceResult.Fail(AccessDeniedMessage);
         }
